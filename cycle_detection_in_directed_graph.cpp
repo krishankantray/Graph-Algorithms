3430bc951dc4d4b4f31b9bf77b72734c5a3ff727
Krishankant <krishankant_rai@rediffmail.com>
2021-11-04 08:27:09 +0530

    Update cycle_detection_in_directed_graph.cpp

--- cycle_detection_in_directed_graph.cpp
+++ cycle_detection_in_directed_graph.cpp
@@ -1,54 +1,64 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <set>
-#include <climits>
-#include <map>
-#include <algorithm>
-#include <cmath>
-#include <utility>
-#include <string>
-#include <cstring>
-#include <deque>
-#include <stack>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define pb push_back
-#define mp make_pair
-#define ull unsigned long long int
 
-bool isCyclic(int s,vector<int>adj[], bool vis[],bool rec[])
-{
-   vis[s]=true;
-   rec[s]=true;
-   for(int i=0; i<adj[s].size(); i++)
-   {
-        if(!vis[adj[s][i]])
-            return isCyclic(adj[s][i],adj,vis,rec);
-        else
-            if(rec[adj[s][i]])
-                return true;
-   }
-   rec[s]=false;
-   return false;
-}
+class Solution {
+private:
+    bool checkCycle(int node, vector<int> adj[], int vis[], int dfsVis[]) {
+        vis[node] = 1; 
+        dfsVis[node] = 1; 
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                if(checkCycle(it, adj, vis, dfsVis)) return true;
+            } else if(dfsVis[it]) {
+                return true; 
+            }
+        }
+        dfsVis[node] = 0; 
+        return false;
+    }
+public:
+	bool isCyclic(int N, vector<int> adj[]) {
+	   int vis[N], dfsVis[N]; 
+	   memset(vis, 0, sizeof vis); 
+	   memset(dfsVis, 0, sizeof dfsVis); 
+	   
+	   for(int i = 0;i<N;i++) {
+	       if(!vis[i]) {
+	           // cout << i << endl; 
+	           if(checkCycle(i, adj, vis, dfsVis)) {
+	               return true; 
+	           }
+	       }
+	   }
+	   return false; 
+	}
+};
+
+
+
 
 int main()
 {
-    int n;
-    cin>>n; // number of vertices
-    vector<int>adj[n];
-    cin.ignore();
-    for(int i=0; i<n; i++)
+    
+    int t;
+    cin >> t;
+    while(t--)
     {
-        string s;
-        getline(cin,s);
-        for(int j=0; j<s.length(); j++)
-            adj[i].push_back((s[j]-'0'));
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.isCyclic(V, adj) << "\n";
     }
-    bool vis[]={false};
-    bool rec[]={false};
-    cout<<isCyclic(0,adj,vis,rec);
+
     return 0;
 }
